4423d7e9a6d9074a92239f563a024cbe24e69358
lethosor <lethosor@gmail.com>
2020-12-25 17:42:14 -0500

    Add 32-bit build matrix entry

--- scripts/build.bash
+++ scripts/build.bash
@@ -11,4 +11,10 @@ if command_exists nproc; then
     JOBS=$(("$(nproc)" + 1))
 fi
 
+if [[ "$RUBY_ARCH" = "32" ]]; then
+    export CFLAGS="$CFLAGS -m32"
+else
+    export CFLAGS="$CFLAGS -m64"
+fi
+
 echo_run make -j"${JOBS}" dll
