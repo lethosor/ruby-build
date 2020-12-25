5f0d362798ff358f1128e8e59bd2378c666c9089
lethosor <lethosor@gmail.com>
2020-12-24 20:19:34 -0500

    Add package script

---
+++ scripts/package.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+. common.inc.bash
+
+set -e
+cd_build
+
+os="$(uname)"
+if [[ "$os" = "Linux" ]]; then
+    dll="$(readlink libruby.so)"
+    out="${dll}.gz"
+    echo_run gzip -kv "$dll"
+else
+    echo "unsupported OS: $(os)"
+    exit 1
+fi
+
+du -h "$dll"
+du -h "$out"
+
+if [[ -n "$1" ]]; then
+    if [[ -d "$1" ]]; then
+        echo_run cp "$out" "$1"
+    else
+        echo "output directory not a directory: $1"
+        exit 1
+    fi
+fi
