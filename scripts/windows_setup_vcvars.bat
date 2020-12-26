64029632e9c5b585762a70a207da7c46fc842e20
lethosor <lethosor@gmail.com>
2020-12-26 12:07:29 -0500

    Fix loop

--- scripts/windows_setup_vcvars.bat
+++ scripts/windows_setup_vcvars.bat
@@ -1,14 +1,15 @@
 @echo off
 rem adapted from https://gist.github.com/vvuk/01dc8a12678d1beffaa1e26549d03b02
 
-set OLDPATH=%PATH%
+set oldpath=%PATH%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 echo INCLUDE='%INCLUDE%'>>%GITHUB_ENV%
 echo LIB='%LIB%'>>%GITHUB_ENV%
 echo LIBPATH='%LIBPATH%'>>%GITHUB_ENV%
 
-for /F "tokens=* delims=;" %%p in ("%%PATH:%OLDPATH%=%%") do (
-    echo new path %%p
-    echo %%p>>%GITHUB_PATH%
+set newpath="%%PATH:%oldpath%=%%"
+for %%p in ("%newpath:;=" "%") do (
+    echo new path %%~p
+    echo %%~p>>%GITHUB_PATH%
 )
