ab876da0ec623987a4469ccbb239e2cf379af98d
lethosor <lethosor@gmail.com>
2020-12-26 11:36:57 -0500

    echo off

--- scripts/windows_setup_vcvars.bat
+++ scripts/windows_setup_vcvars.bat
@@ -1,4 +1,6 @@
+@echo off
 rem adapted from https://gist.github.com/vvuk/01dc8a12678d1beffaa1e26549d03b02
+
 echo %PATH%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
