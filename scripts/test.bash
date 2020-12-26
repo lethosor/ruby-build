8fe223ac28ca958160f85e30dfa7b581395959d2
lethosor <lethosor@gmail.com>
2020-12-26 16:59:35 -0500

    Reduce tests on Windows
    4ddc5a66a8 failed either because a temp directory wasn't cleared properly or Encoding::UTF_8 wasn't defined (although it is
    defined when run in the standalone `miniruby` that was just built). Running `test-knownbug` produces the second and third chunks
    below without exiting unsuccessfully, and inspecting the temp directory lists some files later, so it is likely the former issue.
    
    ```
    D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1460:in `rmdir': Directory not empty @ dir_s_rmdir - C:/Users/RUNNER~1/AppData/Local/Temp/bootstraptest20201226-6432-1dowzlw.tmpwd (Errno::ENOTEMPTY)
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1460:in `block in remove_dir1'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1474:in `platform_support'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1459:in `remove_dir1'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1452:in `remove'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:780:in `block in remove_entry'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1509:in `ensure in postorder_traverse'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1509:in `postorder_traverse'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:778:in `remove_entry'
        from D:/a/ruby-build/ruby-build/ruby/lib/tmpdir.rb:97:in `ensure in mktmpdir'
        from D:/a/ruby-build/ruby-build/ruby/lib/tmpdir.rb:97:in `mktmpdir'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:507:in `in_temporary_working_directory'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:168:in `main'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:530:in `<main>'
    D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1293:in `entries': uninitialized constant Encoding::UTF_8 (NameError)
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:1502:in `postorder_traverse'
        from D:/a/ruby-build/ruby-build/ruby/lib/fileutils.rb:778:in `remove_entry'
        from D:/a/ruby-build/ruby-build/ruby/lib/tmpdir.rb:97:in `ensure in mktmpdir'
        from D:/a/ruby-build/ruby-build/ruby/lib/tmpdir.rb:97:in `mktmpdir'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:507:in `in_temporary_working_directory'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:168:in `main'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:530:in `<main>'
    D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:220:in `exit': exit (SystemExit)
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:220:in `exec_test'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:169:in `block in main'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:509:in `block (2 levels) in in_temporary_working_directory'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:508:in `chdir'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:508:in `block in in_temporary_working_directory'
        from D:/a/ruby-build/ruby-build/ruby/lib/tmpdir.rb:89:in `mktmpdir'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:507:in `in_temporary_working_directory'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:168:in `main'
        from D:/a/ruby-build/ruby-build/ruby/bootstraptest/runner.rb:530:in `<main>'
    ```
    
    Inspecting the temp folder in a test build:
    ```
    ==> ls /c/Users/runneradmin/AppData/Local/Temp/bootstraptest20201226-5520-1hv4330.tmpwd
    a
    assert_normal_exit.log
    b
    bootstraptest.tmp.rb
    require-lock-test.rb
    zzz.rb
    ```
    No indication as to why this isn't cleared.

--- scripts/test.bash
+++ scripts/test.bash
@@ -9,5 +9,6 @@ cd_build
 if is_unix; then
     echo_run make test
 else
-    echo_run nmake test
+    echo_run test "$(./miniruby -e 'puts RUBY_VERSION')" = "$RUBY_VERSION"
+    echo_run nmake test-basic
 fi
