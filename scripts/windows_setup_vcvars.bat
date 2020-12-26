6c45827ca266ec823a2f419daca42d22f569f59a
lethosor <lethosor@gmail.com>
2020-12-26 12:08:51 -0500

    Loop over entire path

--- scripts/windows_setup_vcvars.bat
+++ scripts/windows_setup_vcvars.bat
@@ -1,15 +1,13 @@
 @echo off
 rem adapted from https://gist.github.com/vvuk/01dc8a12678d1beffaa1e26549d03b02
 
-set oldpath=%PATH%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 echo INCLUDE='%INCLUDE%'>>%GITHUB_ENV%
 echo LIB='%LIB%'>>%GITHUB_ENV%
 echo LIBPATH='%LIBPATH%'>>%GITHUB_ENV%
 
-set newpath="%%PATH:%oldpath%=%%"
-for %%p in ("%newpath:;=" "%") do (
+for %%p in ("%PATH:;=" "%") do (
     echo new path %%~p
     echo %%~p>>%GITHUB_PATH%
 )
