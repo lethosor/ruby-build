69303cef0c80f1f88b4bdd4cf5db03328729e0b1
lethosor <lethosor@gmail.com>
2020-12-26 11:30:56 -0500

    Attempt to set up vcvars with a standalone batch script

---
+++ scripts/windows_setup_vcvars.bat
@@ -0,0 +1,8 @@
+rem adapted from https://gist.github.com/vvuk/01dc8a12678d1beffaa1e26549d03b02
+echo %PATH%
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
+echo INCLUDE='%INCLUDE%'>>%GITHUB_ENV%
+echo LIB='%LIB%'>>%GITHUB_ENV%
+echo LIBPATH='%LIBPATH%'>>%GITHUB_ENV%
+echo %PATH%
