5b18a4971a43840576b0ff43f79f6c16206ccc60
lethosor <lethosor@gmail.com>
2020-12-26 14:02:27 -0500

    Revert "Install winflexbison"
    Not needed with source tarball
    
    This reverts commit 466f51f6f14ad1f30054477ebeb4658ec1c324f6.

--- scripts/install_deps.bash
+++ scripts/install_deps.bash
@@ -10,6 +10,4 @@ if is_linux; then
     if [[ "$RUBY_ARCH" = "32" ]]; then
         echo_run sudo apt-get install gcc-multilib g++-multilib
     fi
-elif is_windows; then
-    echo_run choco install winflexbison
 fi
